ec7f1e36bc99fdb79e2e8cc4c9b4d78bbc57a358
George Dautov <infirit89>
2024-01-17 10:51:10 +0200

    added architecture to premake file

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,8 @@ project "yaml-cpp"
     language "C++"
     staticruntime "on"
     
+    architecture "x86_64"
+
     targetdir ("%{prj.location}/bin/" .. outputdir)
     objdir ("%{prj.location}/bin-int/" .. outputdir)
 
